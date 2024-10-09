1273f542e4770087f8e5f74846a24e0a0d878235
shuaimu <shuai@cs.stonybrook.edu>
2024-10-09 13:31:38 -0400

    update

---
+++ acmdownload.py
@@ -0,0 +1,274 @@
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+import json
+import requests
+import sqlite3
+import re
+
+FILE_NAME = 'docs.json'
+DB_NAME = 'docs.db'
+
+def load_docs_file():
+    try:
+        with open(FILE_NAME, 'r') as f:
+            return json.load(f)
+    except:
+        return {}
+
+def save_docs_file(docs):
+    with open(FILE_NAME, 'w') as f:
+        json.dump(docs, f)
+
+def copy_file_to_db():
+    docs = load_docs_file()
+    con = sqlite3.connect(DB_NAME)
+    cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS docs (uid TEXT NOT NULL PRIMARY KEY, doc TEXT NOT NULL)')
+    con.commit()
+    for uid in docs:
+        doc = docs[uid]
+        cur.execute('INSERT INTO docs (uid, doc) VALUES (?, ?)', (uid, json.dumps(doc, ensure_ascii = False)))
+    con.commit()
+    con.close()
+
+def load_docs():
+    docs = {}
+    con = sqlite3.connect(DB_NAME)
+    cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS docs (uid TEXT NOT NULL PRIMARY KEY, doc TEXT NOT NULL)')
+    con.commit()
+    cur.execute('SELECT uid, doc FROM docs')
+    for row in cur.fetchall():
+        docs[row[0]] = json.loads(row[1])
+    con.close()
+    return docs
+
+def save_doc(uid, doc):
+    con = sqlite3.connect(DB_NAME)
+    cur = con.cursor()
+    cur.execute('INSERT INTO docs (uid, doc) VALUES (?, ?)', (uid, json.dumps(doc, ensure_ascii = False)))
+    con.commit()
+    con.close()
+
+class PageParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.in_title = False
+        self.in_references_item = False
+        self.refs = []
+        self.cbu = None
+        self.title = None
+
+    def handle_starttag(self, tag, attrs):
+        d = dict(attrs)
+        if tag == 'h1' and 'citation__title' in d['class']:
+            self.in_title = True
+        elif tag == 'a' and 'data-ajaxurl' in d:
+            cited_by_url = d['data-ajaxurl']
+            if cited_by_url.startswith('/action/ajaxShowCitedBy'):
+                self.cbu = cited_by_url
+        elif self.in_references_item:
+            if tag == 'a':
+                link = d['href']
+                if link.startswith('https://dl.acm.org/doi/'):
+                    link = link[23:]
+                    self.refs.append(link)
+        else:
+            if tag == 'li' and 'class' in d and 'references__item' in d['class']:
+                self.in_references_item = True
+
+    def handle_endtag(self, tag):
+        if self.in_title:
+            self.in_title = False
+        elif self.in_references_item:
+            if tag == 'li':
+                self.in_references_item = False
+
+    def handle_data(self, data):
+        if self.in_title:
+            self.title = data
+
+class CitationParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.links = []
+
+    def handle_starttag(self, tag, attrs):
+        d = dict(attrs)
+        if tag == 'a':
+            link = d['href']
+            if link.startswith('https://doi.org/'):
+                link = link[16:]
+                self.links.append(link)
+
+def search_doc(query):
+    r = requests.get('https://dl.acm.org/action/doSearch', params={'AllField':query})
+    parsed_html = BeautifulSoup(r.text, 'html.parser')
+    t = parsed_html.body.find('div', attrs={'class':'issue-item__content'}).text
+    res = re.findall('org.*', t)
+    doi = res[0][4:]
+    print(doi)
+    r = requests.post('https://dl.acm.org/action/exportCiteProcCitation', data={
+        'dois': doi,
+        'targetFile': 'custom-bibtex',
+        'format': 'bibTex'
+    })
+    j = json.loads(r.text)
+    kvs = list(j['items'][0].items())[0][1]
+    print(kvs)
+
+def download_doc(doi):
+    r = requests.get('https://dl.acm.org/doi/' + doi)
+    page_parser = PageParser()
+    page_parser.feed(r.text)
+
+    doc = {'references': page_parser.refs}
+
+    if page_parser.title:
+        doc['title'] = page_parser.title
+
+    if page_parser.cbu:
+        r = requests.get('https://dl.acm.org' + page_parser.cbu)
+        citation_parser = CitationParser()
+        citation_parser.feed(r.text)
+        doc['citedby'] = citation_parser.links
+    else:
+        doc['citedby'] = []
+
+    r = requests.post('https://dl.acm.org/action/exportCiteProcCitation', data={
+        'dois': doi,
+        'targetFile': 'custom-bibtex',
+        'format': 'bibTex'
+    })
+
+    j = json.loads(r.text)
+    kvs = list(j['items'][0].items())[0][1]
+
+    if 'title' not in doc and 'title' in kvs:
+        doc['title'] = kvs['title']
+
+    for k in ['original-date', 'issued']:
+        if k in kvs:
+            date = kvs[k]
+            ds = map(str, date['date-parts'][0])
+            doc['date'] = '/'.join(ds)
+            break
+
+    if 'author' in kvs:
+        l = []
+        for a in kvs['author']:
+            name = ''
+            if 'given' in a:
+                name += a['given']
+            if 'family' in a:
+                if name:
+                    name += ' '
+                name += a['family']
+            l.append(name)
+        doc['authors'] = ', '.join(l)
+
+    return doc
+
+def get_top_ranked(docs, missing):
+    r = {uid:0 for uid in missing}
+    c = {uid:0 for uid in missing}
+    for doc in docs.values():
+        for uid in doc['references']:
+            if uid in r:
+                r[uid] = r[uid] + 1
+        for uid in doc['citedby']:
+            if uid in c:
+                c[uid] = c[uid] + 1
+    queue = set()
+    queue.add(max(r.items(), key = lambda x: x[1])[0])
+    queue.add(max(c.items(), key = lambda x: x[1])[0])
+    return queue
+
+def download(orig, num_docs):
+    all_docs = load_docs()
+
+    docs = {}
+    uids = set([orig])
+    queue = set([orig])
+
+    while len(docs) < num_docs:
+        if len(queue) == 0:
+            missing = uids - set(docs.keys())
+            if len(missing) == 0:
+                print('No documents are missing, stopping.')
+                exit()
+            queue |= get_top_ranked(docs, missing)
+
+        uid = queue.pop()
+        if uid in all_docs:
+            print('%s: Taking %s from store' % (len(docs) + 1, uid))
+            doc = all_docs[uid]
+        else:
+            print('%s: Downloading %s...' % (len(docs) + 1, uid))
+            doc = download_doc(uid)
+            all_docs[uid] = doc
+            save_doc(uid, doc)
+            #save_docs(all_docs)
+
+        docs[uid] = doc
+        doc_uids = set(doc['references']) | set(doc['citedby'])
+        uids |= doc_uids
+
+        if uid == orig:
+            queue |= doc_uids - set([orig]) # Possibly unnecessary, to guard against paper that references itself.
+
+    return docs
+
+def mostreferenced(docs, orig):
+    rank = {uid:0 for uid in docs}
+
+    for doc in docs.values():
+        for uid in set(doc['references']):
+            if uid in rank:
+                rank[uid] = rank[uid] + 1
+
+    rank = sorted(rank.items(), key = lambda x: -x[1])
+
+    for i in range(min(100, len(docs))):
+        uid, cnt = rank[i]
+        doc = docs[uid]
+        print('%3d, %8s, %3d (%3d), %s. %s: %s' % (i + 1, uid, cnt, len(doc['citedby']), doc['date'] if 'date' in doc else '??/??/????', doc['authors'] if 'authors' in doc else '???', doc['title'] if 'title' in doc else '???'))
+
+def info(doc):
+    for (key, value) in doc.items():
+        if key not in ['references', 'citedby']:
+            print('%s: %s' % (key, value))
+    print('references: %s' % len(doc['references']))
+    print('cited by: %s' % len(doc['citedby']))
+
+def remove_uid_file(uid):
+    docs = load_docs_file()
+    if uid in docs:
+        del docs[uid]
+    save_docs_file(docs)
+
+def remove_uid(uid):
+    con = sqlite3.connect(DB_NAME)
+    cur = con.cursor()
+    cur.execute('DELETE FROM docs WHERE uid=?', (uid,))
+    con.commit()
+    con.close()
+
+def remove_missing_meta():
+    docs = load_docs()
+    uids = list(docs.keys())
+    for uid in uids:
+        doc = docs[uid]
+        if len(set(doc.keys()) - set(['references', 'citedby'])) == 0:
+            remove_uid(uid)
+            del docs[uid]
+    #save_docs(docs)
+
+# doi = '10.5555/2387880.2387905'
+# documents_to_download = 300
+# docs = download(doi, documents_to_download)
+# info(docs[doi])
+# mostreferenced(docs, doi)
+
+test_title = "Building a replicated logging system with Apache Kafka"
+search_doc(test_title)
---
+++ bib-beautify.py
@@ -0,0 +1,158 @@
+
+import bibtexparser
+import glob
+import os 
+from fuzzywuzzy import fuzz
+from bs4 import BeautifulSoup
+import json
+import requests
+import sqlite3
+import re
+import time
+import sys
+
+# target_data = open("../23large/ref.bib")
+# target_data = open("../geolis-paper/ref.bib")
+SRC_FILE = "../warbler-paper/ref.bib"
+SRC_FILE = "/Users/shuai/git/causalmesh-paper/paper.bib"
+# SRC_FILE = "../nsf-23-serverless/ref.bib"
+TGT_FILE = SRC_FILE
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(sys.argv[0])) + '/'
+
+
+n = len(sys.argv);
+if n > 1: 
+    SRC_FILE=sys.argv[1]
+TGT_FILE = SRC_FILE.replace('.bib', '-beautified.bib')
+if n > 2:
+    TGT_FILE=sys.argv[2]
+
+
+def search_doc(query):
+    time.sleep(10)
+    kvs = {}
+    try:
+        r = requests.get('https://dl.acm.org/action/doSearch', params={'AllField':query})
+        parsed_html = BeautifulSoup(r.text, 'html.parser')
+        t = parsed_html.body.find('div', attrs={'class':'issue-item__content'}).text
+        res = re.findall('org.*', t)
+        doi = res[0][4:]
+        print(doi)
+        r = requests.post('https://dl.acm.org/action/exportCiteProcCitation', data={
+            'dois': doi,
+            'targetFile': 'custom-bibtex',
+            'format': 'bibTex'
+        })
+        j = json.loads(r.text)
+        kvs = list(j['items'][0].items())[0][1]
+    except:
+        print("Error searching ACM")
+        pass
+    # print(kvs)
+    return kvs
+
+def json_to_bib(j):
+    pass
+
+# files = list(set(glob.glob(SCRIPT_DIR+'*.bib')) - set(glob.glob(SCRIPT_DIR+"title*.bib")))
+# files.insert(0, SCRIPT_DIR+"title.bib")
+files = list(set(glob.glob('*.bib')) - set(glob.glob("title*.bib")))
+files.insert(0, "title.bib")
+
+data = ""
+for f in files:
+    data += open(f).read()
+# d1 = open("title_short.bib", "r").read()
+# d2 = open("osdi.bib").read()
+bib_database = bibtexparser.loads(data)
+# print(bib_database.entries)
+
+target_data = open(SRC_FILE)
+# target_data = open("../application/cv/ref.bib")
+target_bib = bibtexparser.load(target_data)
+# print(target_bib.entries)
+
+bib_list = []
+for entry in target_bib.entries: 
+    results = []
+    result = entry
+    r = 0
+    title = ""
+    if "title" in entry.keys():
+        title = entry["title"]
+        for e2 in bib_database.entries:
+            r = fuzz.token_set_ratio(entry["title"], e2["title"]);
+            if r == 100:
+                results.append(e2);
+                print("Found match ratio, " + str(r) + ": " + title)
+    if len(results) > 0:
+        result = results[0]
+    else:
+        print("Nothing found for: " + title)
+        writer = bibtexparser.bwriter.BibTexWriter()
+        db = bibtexparser.bibdatabase.BibDatabase()
+        db.entries = [entry]
+        print(writer.write(db))
+        
+        journal = "" 
+        if "journal" in entry:
+            journal = entry["journal"].lower()
+        if "booktitle" in entry:
+            journal = entry["booktitle"].lower()
+        # if False:
+        if (entry["ENTRYTYPE"] == "article" or entry["ENTRYTYPE"] == "inproceedings") and ("vldb" in journal or "sigmod" in journal):
+            print("Search ACM database for it")
+            acm_res = search_doc(title)
+            result = {}
+            result["ENTRYTYPE"] = entry["ENTRYTYPE"]
+            result["ID"] = entry["ID"]
+            result["title"] = entry["title"]
+            result["author"] = entry["author"]
+            if len(acm_res) > 0 and fuzz.token_set_ratio(acm_res['title'], title) == 100:
+                print("Found on ACM")
+                if acm_res["type"] == "PAPER_CONFERENCE":
+                    result["ENTRYTYPE"] = "inproceedings"
+                    result["booktitle"] = acm_res["container-title"]
+                elif acm_res["type"] == "ARTICLE":
+                    result["ENTRYTYPE"] = "article"
+                    result["journal"] = acm_res["container-title"]
+                    result["volume"] = acm_res["volume"]
+                    result["number"] = acm_res["issue"]
+                else:
+                    print("Error")
+                result["year"] = "{}".format(acm_res["issued"]["date-parts"][0][0])
+                result["month"] = "{}".format(acm_res["issued"]["date-parts"][0][1])
+                db.entries = [result]
+                print(writer.write(db))
+            else:
+                print("Didn't find exact match on ACM")
+                result = entry
+    if len(results) > 1: 
+        for e2 in results:
+            if entry["ENTRYTYPE"] == e2["ENTRYTYPE"]:
+                result = e2
+    ids = []
+    if "ids" in result.keys():
+        ids+=result["ids"].split(",")
+    ids.append(entry["ID"]) 
+    if "ids" in entry.keys():
+        ids += entry["ids"].split(",")
+    ids = [*(set(ids)-set([result["ID"]]))]
+    if len(ids) > 0:
+        result["ids"] = ",".join(ids)
+    else:
+        result.pop('ids', None)
+    result["ID"] = entry["ID"]
+    bib_list.append(result) 
+
+
+res_db = bibtexparser.bibdatabase.BibDatabase()
+result_entries = []
+[result_entries.append(x) for x in bib_list if x not in result_entries] 
+res_db.entries = result_entries
+
+
+with open(TGT_FILE, 'w') as bibtex_file:
+    bibtexparser.dump(res_db, bibtex_file)
+
